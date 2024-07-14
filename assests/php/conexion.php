a9b92221251646d7d488be7c7b6ed6260919eb20
Pichi <dsjoseale@gmail.com>
2024-07-14 19:54:12 -0400

    gay lein

--- assests/php/conexion.php
+++ assests/php/conexion.php
@@ -1,15 +1,23 @@
 <?php
 
 session_start();
+    //Docker
+    $db_host = getenv('DB_HOST'); 
+    $db_user = getenv('DB_USER'); 
+    $db_password = getenv('DB_PASSWORD'); 
+    $db_database = getenv('DB_DATABASE'); 
 
-    $db_host = 'localhost'; 
-    $db_user = 'root'; 
-    $db_password = ''; 
-    $db_database = 'pichi'; 
+    $db_port = getenv('DB_PORT');
 
+    if ($db_host === false || $db_user === false || $db_password === false || $db_database === false){
+        $db_host = 'localhost'; 
+        $db_user = 'root'; 
+        $db_password = ''; 
+        $db_database = 'pichi';
+    }
 
-$mysqli = mysqli_connect($db_host, $db_user, $db_password, $db_database);
-
+$mysqli = mysqli_connect($db_host, $db_user, $db_password, $db_database, $db_port);
+ 
 
 
 ?>
